bb4bb9e6c4fee6cd8717d9c29a5106d7853e23bf
GSilv-a <guilhermeassi0009@gmail.com>
2025-09-08 21:22:18 -0300

    manutenções

---
+++ BACK AND/DOG/dog.js
@@ -0,0 +1,21 @@
+var dogimg = document.getElementById("dogimg");
+var botao = document.getElementById("botao");
+
+//função que busca imagem de dog
+async function buscar(){
+    //try vai rodar app normalmente
+    try {
+        //faz a busca do link da api
+        var response = await fetch("https://dog.ceo/api/breeds/image/random");
+        //armazena o conteúdo
+        var dados = await response.json();
+        //joga na imagem o link da foto
+        dogimg.src= dados.message;
+
+    }catch(error){
+        //catch serve para capturar erro se der errado
+        alert("Erro ao buscar dog!", error);
+    }
+}
+//se tiver evento de click no botão chama a função buscar
+botao.addEventListener("click", buscar);
--- BACK AND/SURICATO/script.js
+++ BACK AND/SURICATO/script.js
@@ -1,10 +1,21 @@
 function buscar(){
-    var cep = 87970000
+    // pegar o cep do input
+    var cep = document.getElementyById('cep').value;
+    // link da api de cep
     var url ="viacep.com.br/ws/01001000/json/"+cep+"/json"
 
+    // faz a busca na api de cep
 fetch(url)
 .then(response => response.json())
-.then(dados => { alert(dados.localidade) });
+.then(dados => { 
+    if(dados.erro){
+        // se tiver erro
+        document.getElementById('resultado').textContent = 'Cep não encontrado!';
+    }else{
+        // se der certo
+        document.getElementById('resultado').innerHTML = '<strong> Cidade: </strong> ' +dados.localidade
+    }
+});
 
 }
 
