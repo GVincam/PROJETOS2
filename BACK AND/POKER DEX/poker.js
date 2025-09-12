fd89094aeea8b3336bd0494750192fea61b73304
GVincam <gvincam@gmail.com>
2025-09-11 21:12:59 -0300

    pokedex

---
+++ BACK AND/POKER DEX/poker.js
@@ -0,0 +1,14 @@
+function buscar(){
+    // GUARDA NA VRIAVEL VALOR DO INPUT
+    var entrada = document.getElementById("entrada").value.toLowerCase();
+
+    // Link da api
+    var url="https://pokeapi.co/api/v2/pokemon/"+entrada;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(dados => {
+        // escrever no html
+        tela.innerHTML = `<h2> ${dados.name} </h2>`;
+    })
+}
