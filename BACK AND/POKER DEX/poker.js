3f2c6d9372e06c31c71b4551423f7bdb05e12746
GVincam <gvincam@gmail.com>
2025-09-15 21:55:08 -0300

    poker dex

--- BACK AND/POKER DEX/poker.js
+++ BACK AND/POKER DEX/poker.js
@@ -1,6 +1,12 @@
+var contador = 0;
+
 function buscar(){
     // GUARDA NA VRIAVEL VALOR DO INPUT
     var entrada = document.getElementById("entrada").value.toLowerCase();
+// Se a entrada for menor que 3 caracteres
+    // if(entrada.length < 1){
+    //     entrada = contador;
+    // }
 
     // Link da api
     var url="https://pokeapi.co/api/v2/pokemon/"+entrada;
@@ -14,6 +20,19 @@ function buscar(){
         <h2> ${dados.name} </h2>
         <img class="pokemon" src="${dados.sprites.front_default}">
         <img class="pokemon" src="${dados.sprites.back_default}">
+        <p> Id: ${dados.id} </p>
+        <p> Tipo: ${dados.types.map(type => type.type.name)} </p>
+        <p> Habilidades: ${dados.abilities.map(ability => ability.ability.name)} </p>
         `;
     })
+}
+
+function proximo(){
+    contador = contador + 1;
+    buscar();
+}
+
+function anterior(){
+    contador = contador - 1;
+    buscar();
 }
