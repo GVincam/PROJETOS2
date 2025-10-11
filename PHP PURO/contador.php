7e5767a912192cc4785031e9103e254fcb79326f
GVincam <gvincam@gmail.com>
2025-10-10 21:44:36 -0300

    continuação

---
+++ PHP PURO/contador.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Contador de Visitas</title>
+</head>
+<body>
+    <div>
+        <h2> Contador de visitas </h2>
+        <p> Aprendendo sobre Cookies(memória armazenada no navegador)
+            e Session(Se)
+        </p>
+    </div>
+    
+</body>
+</html>
