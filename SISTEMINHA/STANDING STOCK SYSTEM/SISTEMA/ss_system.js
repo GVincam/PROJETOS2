3e8ba0c67212c5ac0dff9ba5f8256cb5ac2debc3
GVincam <gvincam@gmail.com>
2026-03-23 21:39:12 -0300

    Salvando

---
+++ SISTEMINHA/STANDING STOCK SYSTEM/SISTEMA/ss_system.js
@@ -0,0 +1,7 @@
+var estoque = document.getElementById("verEstoque");
+var caixaDeOpcao = document.querySelector(".caixaDeOpcao");
+
+estoque.addEventListener("click", function(){
+     caixaDeOpcao.classList.add("ativo");
+
+});
