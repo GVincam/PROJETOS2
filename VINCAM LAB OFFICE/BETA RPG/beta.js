ad0efdba5433385ddaa50326f39d653bb34fced7
GVincam <gvincam@gmail.com>
2026-05-11 21:50:59 -0300

    Salvar

---
+++ VINCAM LAB OFFICE/BETA RPG/beta.js
@@ -0,0 +1,14 @@
+let personagem = {
+    nome: "Inis Satômi",
+    vida: 100,
+    nivel: 1,
+    classe: "Maga",
+    habilidade: "Chamas Molecular",
+    atributos: {
+        forca: 15,
+        agilidade: 15,
+        inteligencia: 50,
+        mana: 100
+    },
+    inventario: []
+}
