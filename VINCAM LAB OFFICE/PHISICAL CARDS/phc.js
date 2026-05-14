a7bb58cd091746b29314be80d95f24f4e3a12e4d
GVincam <gvincam@gmail.com>
2026-05-13 21:51:26 -0300

    salvar

---
+++ VINCAM LAB OFFICE/PHISICAL CARDS/phc.js
@@ -0,0 +1,10 @@
+function mostrarBatalha(){
+      let campoBatalha = document.getElementById("campoBatalha").style.display = "block";
+
+      let mensage1 = document.getElementById("mesage1").innerText = "A Batalha vai começar!!!";
+
+    }
+
+ let cartas = ["carta1", "carta2", "carta3", "carta4", "carta5", "carta6"]
+
+    
