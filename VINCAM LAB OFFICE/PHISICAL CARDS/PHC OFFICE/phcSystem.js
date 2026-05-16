a5036074cc80b3175c52aacfcfd8ca95844e144d
GVincam <gvincam@gmail.com>
2026-05-15 22:02:35 -0300

    salvar

---
+++ VINCAM LAB OFFICE/PHISICAL CARDS/PHC OFFICE/phcSystem.js
@@ -0,0 +1,12 @@
+function mostrarTutorial(){
+  let tutorial = document.getElementById("tutorial").style.display = "block";
+}
+
+function jogar(){
+  
+  let painel = document.getElementById("painel").style.display = "block";
+  
+  let painelCartas = document.getElementById("painelCartas").style.display = ("block");
+  
+  let abrirJogo = document.getElementById("abrirJogo").style.display = "none";
+}
