fd8d33e7e8f0fb837b98603a8c14ca51de353ef9
GVincam <gvincam@gmail.com>
2026-06-09 21:56:37 -0300

    salvar

--- VINCAM LAB OFFICE/PROJETO PORTO/INTERAÇÕES/cardPorto.js
+++ VINCAM LAB OFFICE/PROJETO PORTO/INTERAÇÕES/cardPorto.js
@@ -1,4 +1,4 @@
- function ampliar(){
+        function ampliar(){
             
             document.body.style.overflow = "hidden"
 
@@ -358,8 +358,8 @@
 
             let text1 = document.createElement("h2")
             text1.classList.add("titleIntro")
+            text1.textContent = "PRAINHAS PORTO MARINGÁ"
             // text1.style.fontSize = "40px"
-            // text1.textContent = "PRAINHAS PORTO MARINGÁ"
             // text1.style.textAlign = "center"
             // text1.style.position = "relative"
             // text1.style.top = "40px"
@@ -459,7 +459,7 @@
             
 
             let description = document.createElement("div")
-            description.classList.add("guardImg")
+            description.classList.add("description")
             // description.style.width = "500px"
             // description.style.height = "500px"
             // description.style.position = "relative"
@@ -506,14 +506,14 @@
 
             let caixaNova = document.createElement("section") 
             caixaNova.classList.add("painelFechado")
-            caixaNova.style.width= "1600px"
-            caixaNova.style.height= "770px"
-            caixaNova.style.position= "fixed"
-            caixaNova.style.top= "50%"
-            caixaNova.style.left= "50%"
-            caixaNova.style.transform = "translate(-50%, -50%)"
-            caixaNova.style.backgroundColor = "rgba(0, 84, 252, 0.4)"
-            caixaNova.style.backdropFilter = "blur(7px)"
+            // caixaNova.style.width= "1600px"
+            // caixaNova.style.height= "770px"
+            // caixaNova.style.position= "fixed"
+            // caixaNova.style.top= "50%"
+            // caixaNova.style.left= "50%"
+            // caixaNova.style.transform = "translate(-50%, -50%)"
+            // caixaNova.style.backgroundColor = "rgba(0, 84, 252, 0.4)"
+            // caixaNova.style.backdropFilter = "blur(7px)"
 
             document.body.appendChild(caixaNova)
 
@@ -524,15 +524,15 @@
 
 
             let text1 = document.createElement("h2")
-            text1.style.fontSize = "40px"
-            text1.textContent = "QUIOSQUE PORTO MARINGÁ"
             text1.classList.add("titleIntro")
-            text1.style.textAlign = "center"
-            text1.style.position = "relative"
-            text1.style.top = "40px"
-            text1.style.fontFamily = "Concert One"
-            text1.style.fontFamily = "sans-serif"
-            text1.style.color = "rgb(255, 220, 0)"
+            text1.textContent = "QUIOSQUE PORTO MARINGÁ"
+            // text1.style.fontSize = "40px"
+            // text1.style.textAlign = "center"
+            // text1.style.position = "relative"
+            // text1.style.top = "40px"
+            // text1.style.fontFamily = "Concert One"
+            // text1.style.fontFamily = "sans-serif"
+            // text1.style.color = "rgb(255, 220, 0)"
 
             caixaNova.appendChild(text1)
 
@@ -541,12 +541,12 @@
 
           let botao = document.createElement("button")
             botao.classList.add("btnBack")
-            botao.style.position = "relative"
-            botao.style.left = "35px"
-            botao.style.top = "10px"
-            botao.style.width = "140px"
-            botao.style.cursor = "pointer"
-            botao.style.borderRadius = "10px"
+            // botao.style.position = "relative"
+            // botao.style.left = "35px"
+            // botao.style.top = "10px"
+            // botao.style.width = "140px"
+            // botao.style.cursor = "pointer"
+            // botao.style.borderRadius = "10px"
 
 
             botao.addEventListener("click", function(){
@@ -568,12 +568,12 @@
             let backArrow = document.createElement("i")
             backArrow.classList.add("fa-solid")
             backArrow.classList.add("fa-arrow-left")
-            backArrow.style.position="relative"
-            backArrow.style.left="-45px"
-            backArrow.style.top="13px"
-            backArrow.style.fontSize="25px"
-            backArrow.style.cursor = "pointer"
-            backArrow.style.color = "white"
+            // backArrow.style.position="relative"
+            // backArrow.style.left="-45px"
+            // backArrow.style.top="13px"
+            // backArrow.style.fontSize="25px"
+            // backArrow.style.cursor = "pointer"
+            // backArrow.style.color = "white"
             
             backArrow.addEventListener("click", function(){
 
@@ -592,12 +592,12 @@
             let voltar = document.createElement("h3")
             voltar.textContent = "VOLTAR"
             voltar.classList.add("voltar")
-            voltar.style.position = "relative"
-            voltar.style.bottom = "10px"
-            voltar.style.left = "20px"
-            voltar.style.fontSize = "20px"
-            voltar.style.cursor = "pointer"
-            voltar.style.color = "white"
+            // voltar.style.position = "relative"
+            // voltar.style.bottom = "10px"
+            // voltar.style.left = "20px"
+            // voltar.style.fontSize = "20px"
+            // voltar.style.cursor = "pointer"
+            // voltar.style.color = "white"
 
             voltar.addEventListener("click", function(){
                 caixaNova.classList.remove("painelLigado")
@@ -614,48 +614,50 @@
 
 
             let guardImg = document.createElement("div")
-            guardImg.style.width="900px"
-            guardImg.style.height="500px"
-            guardImg.style.position = "relative"
-            guardImg.style.top = "50px"
-            guardImg.style.left = "20px"
+            guardImg.classList.add("guardImg")
+            // guardImg.style.width="900px"
+            // guardImg.style.height="500px"
+            // guardImg.style.position = "relative"
+            // guardImg.style.top = "50px"
+            // guardImg.style.left = "20px"
 
             caixaNova.appendChild(guardImg)
 
             
 
             let description = document.createElement("div")
-            description.style.width = "500px"
-            description.style.height = "500px"
-            description.style.position = "relative"
-            description.style.left = "970px"
-            description.style.bottom = "450px"
-            description.style.alignContent = "center"
-            description.style.justifyContent = "center"
+            description.classList.add("description")
+            // description.style.width = "500px"
+            // description.style.height = "500px"
+            // description.style.position = "relative"
+            // description.style.left = "970px"
+            // description.style.bottom = "450px"
+            // description.style.alignContent = "center"
+            // description.style.justifyContent = "center"
 
             caixaNova.appendChild(description)
 
 
 
             let text2 = document.createElement("h1")
-            text2.style.fontSize = "21px"
-
+            text2.classList.add("descriptionIntro")
             text2.textContent = 'Os quiosques de Porto Maringá complementam a experiência turística da região, oferecendo conforto e praticidade aos visitantes que frequentam as margens do Rio Paraná. Localizados próximos às áreas de lazer e às prainhas, esses espaços servem como ponto de encontro para famílias, amigos e turistas durante todo o ano. Além de proporcionarem sombra e descanso, os quiosques são frequentemente utilizados para confraternizações, refeições e momentos de contemplação da paisagem natural. Sua presença contribui para a infraestrutura turística do porto, tornando a permanência dos visitantes mais agradável e organizada. Com vista privilegiada para o rio e para os belos pôres do sol da região, os quiosques se tornaram parte importante da identidade turística de Porto Maringá.'
 
-            text2.classList.add("descriptionIntro")
-            text2.style.textAlign = "center"
-            text2.style.color = "white"
-            text2.style.fontFamily = "Nunito"
-            text2.style.fontFamily = "sans-serif"
+            // text2.style.fontSize = "21px"
+            // text2.style.textAlign = "center"
+            // text2.style.color = "white"
+            // text2.style.fontFamily = "Nunito"
+            // text2.style.fontFamily = "sans-serif"
 
             description.appendChild(text2)
 
             
 
             let imageGrande = document.createElement("img")
+            imageGrande.classList.add("imageGrande")
             imageGrande.src = "images/QUIOSQUE.JPG"
             imageGrande.alt = "quiosquePorto"
-            imageGrande.style.borderRadius = "10px"
+            // imageGrande.style.borderRadius = "10px"
 
             
             guardImg.appendChild(imageGrande)
